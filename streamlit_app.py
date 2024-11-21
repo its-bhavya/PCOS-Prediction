5fd96d4c2dcdedfeec37ba92607ef0dacb013a99
Bhavya <142643436+its-bhavya@users.noreply.github.com>
2024-11-21 19:34:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ y = df.PCOS
 
 with st.sidebar:
   st.header("Input Features")
-  '''Age (yrs),Weight (Kg),Height(Cm) ,BMI,Pulse rate(bpm) ,Hb(g/dl),Cycle length(days),FSH(mIU/mL),Hip(inch),
+  Height(Cm) ,BMI,Pulse rate(bpm) ,Hb(g/dl),Cycle length(days),FSH(mIU/mL),Hip(inch),
   Waist(inch),AMH(ng/mL),Follicle No. (L),Follicle No. (R),Avg. F size (L) (mm),Avg. F size (R) (mm),Blood Group,
   Cycle(R/I),Pregnant(Y/N),Weight gain(Y/N),hair growth(Y/N),Skin darkening (Y/N),Hair loss(Y/N),Pimples(Y/N),
   Fast food (Y/N),Reg.Exercise(Y/N)'''
