fe409c3c747073613c1241c8a75509ad1c0f81f2
Bhavya <142643436+its-bhavya@users.noreply.github.com>
2024-11-21 21:52:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,12 @@ df = pd.read_csv("https://raw.githubusercontent.com/its-bhavya/PCOS-Prediction/r
 
 X = df.drop("PCOS", axis = 1)
 y = df.PCOS
-
-with st.sidebar:
-  st.header("Input Features")
-  #Pulse rate(bpm) ,Hb(g/dl),Cycle length(days),FSH(mIU/mL),Hip(inch),
+#Pulse rate(bpm) ,Hb(g/dl),Cycle length(days),FSH(mIU/mL),Hip(inch),
   #Waist(inch),AMH(ng/mL),Follicle No. (L),Follicle No. (R),Avg. F size (L) (mm),Avg. F size (R) (mm)
-  
-  Age = st.slider("How old are you?", 14, 50, 25)
-  Weight = st.slider("How much do you weigh (In Kg)", 25, 150, 60)
-  Height = st.slider("How tall are you? (In CM)", 100, 220, 155)
-  BMI = st.text_input("Please enter your BMI.", 24.55)
+Age = st.slider("How old are you?", 14, 50, 25)
+Weight = st.slider("How much do you weigh (In Kg)", 25, 150, 60)
+Height = st.slider("How tall are you? (In CM)", 100, 220, 155)
+BMI = st.text_input("Please enter your BMI.", 24.55)
   BloodGroup = st.selectbox("What's your blood group?", ("A+","B+","AB+", "O+", "O-", "A-", "B-", "AB-"))
   CycleReg = st.radio("Is your menstrual cycle regular?", ("Yes", "No"))
   WeightGain = st.radio("Have you experienced recent weight gain?", ("Yes", "No"))
@@ -28,6 +24,7 @@ with st.sidebar:
   Pimples = st.radio("Have you been struggling with acne or pimples?", ("Yes", "No"))
   FastFood = st.radio("Do you regularly consume fast food? (More than three times a week)", ("Yes", "No"))
   RegExercise = st.radio("Do you exercise regularly?", ("Yes", "No"))
-
+  
+  
   
   
