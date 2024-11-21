b234275d60df78167a87355e32996da32614156a
Bhavya <142643436+its-bhavya@users.noreply.github.com>
2024-11-21 19:14:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Hormoniq')
 
 st.info('This app can help you predict whether or not you have PCOS.')
+
+df = pd.read_csv("https://raw.githubusercontent.com/its-bhavya/PCOS-Prediction/refs/heads/master/Final_PCOS_Dataset.csv")
