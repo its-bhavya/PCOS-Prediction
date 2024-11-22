759a5721661c8163f224f36a3f7273b1ea27c759
Bhavya <142643436+its-bhavya@users.noreply.github.com>
2024-11-22 12:01:05 +0530

    Update streamlit_app.py
    Added all input widgets

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,11 @@ df = pd.read_csv("https://raw.githubusercontent.com/its-bhavya/PCOS-Prediction/r
 
 X = df.drop("PCOS", axis = 1)
 y = df.PCOS
-#Pulse rate(bpm) ,Hb(g/dl),Cycle length(days),FSH(mIU/mL),Hip(inch),
-  #Waist(inch),AMH(ng/mL),Follicle No. (L),Follicle No. (R),Avg. F size (L) (mm),Avg. F size (R) (mm)
+#Follicle No. (L),Follicle No. (R),Avg. F size (L) (mm),Avg. F size (R) (mm)
 Age = st.slider("How old are you?", 14, 50, 25)
 Weight = st.slider("How much do you weigh (In Kg)", 25, 150, 60)
 Height = st.slider("How tall are you? (In CM)", 100, 220, 155)
-BMI = st.text_input("Please enter your BMI.", 24.55)
+BMI = st.number_input("Please enter your BMI.", 24.55)
 BloodGroup = st.selectbox("What's your blood group?", ("A+","B+","AB+", "O+", "O-", "A-", "B-", "AB-"))
 CycleReg = st.radio("Is your menstrual cycle regular?", ("Yes", "No"))
 WeightGain = st.radio("Have you experienced recent weight gain?", ("Yes", "No"))
@@ -24,7 +23,18 @@ HairLoss = st.radio("Have you experienced recent Hair Loss?", ("Yes", "No"))
 Pimples = st.radio("Have you been struggling with acne or pimples?", ("Yes", "No"))
 FastFood = st.radio("Do you regularly consume fast food? (More than three times a week)", ("Yes", "No"))
 RegExercise = st.radio("Do you exercise regularly?", ("Yes", "No"))
-  
-  
+Pulse = st.number_input("Enter your pulse rate: ", 50, 100, 72)
+Haemoglobin = st.number_input("Please enter your Haemoglobin Levels (in g/dL)", 1, 30, 11)
+CycleLength = st.slider("How long does your period last on an average?", 2, 8, 5)
+Hip = st.slider("Please enter your hip measurements (in Inches): ", 24, 56,  38)
+Waist = st.slider("Please enter your waist measurements (in Inches): ",20, 56, 34)
+FSH = st.number_input("Enter your FSH levels (in mIU/mL): ", 12.5)
+AMH = st.number_input("Enter your AMH levels (in ng/mL): ", 6)
+LeftFollicleNumbers = st.number_input("Enter your Left Follicle Number: ", 6)
+RightFollicleNumbers = st.number_input("Enter your Right Follicle Number: ", 6)
+AvgRFollicleSize = st.number_input("Enter your average right follicle size: ", 15)
+AvgLFollicleSize = st.number_input("Enter your average left follicle size:", 15)
+
+
   
   
