6955fc2c22c23da117f4109b79ec967107f5d392
its-bhavya <bhavyaa716@gmail.com>
2024-11-22 14:28:54 +0530

    Updated model_training.py

--- model_training.py
+++ model_training.py
@@ -32,6 +32,7 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 results = {}
 
 for name, clf in classifiers.items():
+    X_train = np.array(X_train)
     clf.fit(X_train, y_train)
     X_test = np.array(X_test)
     y_pred = clf.predict(X_test)
@@ -148,60 +149,3 @@ print(f'Recall: {recall:.4f}')
 print(f'F1: {f1:.4f}')
 print("Confusion Matrix\n", conf_matrix)
 
-from sklearn.ensemble import AdaBoostClassifier
-base_dt = DecisionTreeClassifier(max_depth = 5, random_state = 42)
-adaboost_clf = AdaBoostClassifier(estimator = base_dt, n_estimators = 50, random_state = 42)
-
-adaboost_clf.fit(X_train, y_train)
-
-from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score, confusion_matrix
-y_pred = best_model.predict(X_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average = 'weighted')
-recall = recall_score(y_test, y_pred, average = 'weighted')
-f1 = f1_score(y_test, y_pred, average = 'weighted')
-conf_matrix = confusion_matrix(y_test, y_pred)
-
-print(f'Accuracy: {accuracy:.4f}')
-print(f'Precision: {precision:.4f}')
-print(f'Recall: {recall:.4f}')
-print(f'F1: {f1:.4f}')
-print("Confusion Matrix\n", conf_matrix)
-
-numerical_selected_indices = f_selector.get_support(indices=True)
-categorical_selected_indices = chi2_selector.get_support(indices=True)
-
-# Get selected feature names for both numerical and categorical
-selected_numerical_features = [numerical_features[i] for i in numerical_selected_indices]
-selected_categorical_features = [categorical_features[i] for i in categorical_selected_indices]
-
-# Combine them into one list
-selected_features = selected_numerical_features + selected_categorical_features
-
-print("Selected Features:")
-print(selected_features)
-
-feature_importances = adaboost_clf.feature_importances_
-
-# Create a DataFrame to display feature names and their importances together
-feature_importance_df = pd.DataFrame({
-    'Feature': selected_features,
-    'Importance': feature_importances
-})
-
-feature_importance_df
-
-plt.figure(figsize=(10, 10))
-plt.barh(selected_features, feature_importances, color = 'navy')
-plt.xlabel("Feature Importance")
-plt.ylabel("Feature Name")
-plt.title("Feature Importance Plot - Adaboost")
-plt.grid(True)
-plt.show()
-
-from sklearn.tree import plot_tree
-plt.figure(figsize =  (50,50))
-plot_tree(best_model, feature_names = selected_features, filled = True, class_names = ["No PCOS", "PCOS"])
-plt.title("Decision Tree Visualization")
-plt.show()
